8a81d2239715a9fb2cd8072b9a09f97d7807626c
EricMTodd <eterictodd@gmail.com>
2021-04-11 14:02:39 -0600

    Basic landing page layout and styling choices established.

--- src/landing-page.js
+++ src/landing-page.js
@@ -2,7 +2,6 @@ const landingPageController = (() => {
 	const createHeader = () => {
 		let header = document.createElement('div');
 		header.id = 'header';
-		console.log('createHeader');
 		document.querySelector('#content').appendChild(header);
 	};
 
@@ -10,7 +9,6 @@ const landingPageController = (() => {
 		let title = document.createElement('div');
 		title.id = 'title';
 		title.innerText = 'The Boozy Badger';
-		console.log('createTitle');
 		document.querySelector('#header').appendChild(title);
 	};
 
@@ -18,15 +16,20 @@ const landingPageController = (() => {
 		let tagline = document.createElement('div');
 		tagline.id = 'tagline';
 		tagline.innerText = 'Fine Brews and Crews';
-		console.log('createTagline');
 		document.querySelector('#header').appendChild(tagline);
 	};
 
 	const createFooter = () => {
 		let footer = document.createElement('div');
 		footer.id = 'footer';
-		footer.innerText = 'Made with ❤️ by Eric M. Todd';
-		console.log('createFooter');
+		let footerSpan = document.createElement('span');
+		footerSpan.innerText = 'Made with ❤️ by ';
+		footerSpan.id = 'footerSpan';
+		footer.appendChild(footerSpan);
+		let gitHubLink = document.createElement('a');
+		gitHubLink.innerText = 'Eric M. Todd';
+		gitHubLink.href = 'https://github.com/EricMTodd';
+		footer.appendChild(gitHubLink);
 		document.querySelector('#content').appendChild(footer);
 	};
 

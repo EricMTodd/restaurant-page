4897dbbc02887474fb38b61cbd76b1a119881d06
EricMTodd <eterictodd@gmail.com>
2021-04-14 00:27:23 -0600

    Committing for good measure.

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ const aboutPageController = (() => {
 	const renderAboutContainer = () => {
 		let container = document.querySelector('#container');
 		if (!document.querySelector('#container-header')) {
-			let containerHeader = document.createElement('h1');
+			let containerHeader = document.createElement('div');
 			containerHeader.id = 'container-header';
 			containerHeader.className = 'primary-text';
 			containerHeader.innerText = 'About Us';
@@ -11,16 +11,19 @@ const aboutPageController = (() => {
 			let containerHeader = document.querySelector('#container-header');
 			containerHeader.innerText = 'About Us';
 		}
-		if (!document.querySelector('#container-body')) {
-			let containerBody = document.createElement('p');
-			containerBody.id = 'container-body';
-			containerBody.className = 'secondary-text';
-			containerBody.innerText = `Located in the Docks District of Morath, The Boozy Badger is a must visit for any sea-weary sailor. With fine drink, food and good company, it's the best spot you can spend your shore leave!`;
-			container.appendChild(containerBody);
-		} else {
-			let containerBody = document.querySelector('#container-body');
-			containerBody.innerText = `Located in the Docks District of Morath, The Boozy Badger is a must visit for any sea-weary sailor. With fine drink, food and good company, it's the best spot you can spend your shore leave!`;
-		}
+		(() => {
+			let bodyText = `Located in the Docks District of Morath, The Boozy Badger is a must visit for any sea-weary sailor. With fine drink, food and good company, it's the best spot you can spend your shore leave!`;
+			if (!document.querySelector('#container-body')) {
+				let containerBody = document.createElement('p');
+				containerBody.id = 'container-body';
+				containerBody.className = 'secondary-text';
+				containerBody.innerText = bodyText;
+				container.appendChild(containerBody);
+			} else {
+				let containerBody = document.querySelector('#container-body');
+				containerBody.innerText = bodyText;
+			}
+		})();
 	};
 	return {
 		renderAboutContainer,

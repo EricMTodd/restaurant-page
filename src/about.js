48db14377ee088bc44ae336e880fcedd427bae25
Eric M. Todd <eterictodd@gmail.com>
2021-04-16 21:08:14 -0600

    Working on positioning content inside of their divs.

--- src/about.js
+++ src/about.js
@@ -1,5 +1,6 @@
 const aboutPageController = (() => {
 	const renderAboutPage = () => {
+		let pageBody = document.querySelector('#page-body');
 		const renderAboutPageTitle = (() => {
 			document.querySelector('#page-title').innerText = 'About us';
 		})();
@@ -9,25 +10,22 @@ const aboutPageController = (() => {
 				let servicesContainer = document.createElement('div');
 				servicesContainer.id = 'services-container';
 				servicesContainer.className = 'secondary-text';
-				document.querySelector('#page-body').appendChild(servicesContainer);
+				pageBody.appendChild(servicesContainer);
 
 				const renderServicesImage = (() => {
 					let servicesImage = document.createElement('img');
 					servicesImage.id = 'services-image';
 					servicesImage.src = '../dist/images/about/drinking-pirates1.jpg';
 					servicesImage.alt = 'services-image';
-					document
-						.querySelector('#services-container')
-						.appendChild(servicesImage);
+					servicesContainer.appendChild(servicesImage);
 				})();
 
 				const renderServicesText = (() => {
 					let servicesText = document.createElement('div');
 					servicesText.id = 'services-text';
-					servicesText.innerText = 'Services';
-					document
-						.querySelector('#services-container')
-						.appendChild(servicesText);
+					servicesText.innerText =
+						'Services are available 24 hours. Room and board with plenty of fine company!';
+					servicesContainer.appendChild(servicesText);
 				})();
 			})();
 
@@ -35,20 +33,43 @@ const aboutPageController = (() => {
 				let eventsContainer = document.createElement('div');
 				eventsContainer.id = 'events-container';
 				eventsContainer.className = 'secondary-text';
-				document.querySelector('#page-body').appendChild(eventsContainer);
+				pageBody.appendChild(eventsContainer);
+
+				const renderEventsText = (() => {
+					let eventsText = document.createElement('div');
+					eventsText.id = 'events-text';
+					eventsText.innerText = 'Events';
+					eventsContainer.appendChild(eventsText);
+				})();
 
 				const renderEventsImage = (() => {
 					let eventsImage = document.createElement('img');
 					eventsImage.id = 'events-image';
 					eventsImage.alt = 'events-image';
-					document.querySelector('#events-container').appendChild(eventsImage);
+					eventsContainer.appendChild(eventsImage);
 				})();
-
-				const renderEventsText = (() => {})();
 			})();
 
 			const renderLocationContainer = (() => {
-				console.log('location');
+				let locationContainer = document.createElement('div');
+				locationContainer.id = 'location-container';
+				locationContainer.className = 'secondary-text';
+				pageBody.appendChild(locationContainer);
+
+				const renderLocationImage = (() => {
+					let locationImage = document.createElement('img');
+					locationImage.id = 'location-image';
+					locationImage.alt = 'location-image';
+					locationContainer.appendChild(locationImage);
+				})();
+
+				const renderLocationText = (() => {
+					let locationText = document.createElement('div');
+					locationText.id = 'location-text';
+					locationText.className = 'secondary-text';
+					locationText.innerText = 'Location';
+					locationContainer.appendChild(locationText);
+				})();
 			})();
 		})();
 	};

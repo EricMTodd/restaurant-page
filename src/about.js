c962e8b79430482db795a7930faec2ba2ae99ad6
Eric M. Todd <eterictodd@gmail.com>
2021-04-19 01:23:52 -0600

    Div styling working as expected.

--- src/about.js
+++ src/about.js
@@ -10,70 +10,24 @@ const aboutPageController = (() => {
 				let servicesContainer = document.createElement('div');
 				servicesContainer.id = 'services-container';
 				servicesContainer.className = 'secondary-text';
+				servicesContainer.innerHTML = `<img id='services-image' src='../dist/images/about/drinking-pirates1.jpg' > Services are available 24 hours. Room and board with plenty of fine company!`;
 				pageBody.appendChild(servicesContainer);
-
-				const renderServicesImage = (() => {
-					let servicesImage = document.createElement('img');
-					servicesImage.id = 'services-image';
-					servicesImage.src = '../dist/images/about/drinking-pirates1.jpg';
-					servicesImage.alt = 'services-image';
-					servicesContainer.appendChild(servicesImage);
-				})();
-
-				const renderServicesText = (() => {
-					let servicesText = document.createElement('div');
-					servicesText.id = 'services-text';
-					servicesText.innerText =
-						'Services are available 24 hours. Room and board with plenty of fine company!';
-					servicesContainer.appendChild(servicesText);
-				})();
 			})();
 
 			const renderEventsContainer = (() => {
 				let eventsContainer = document.createElement('div');
 				eventsContainer.id = 'events-container';
 				eventsContainer.className = 'secondary-text';
+				eventsContainer.innerHTML = `<img id='events-image' src='../dist/images/about/drinking-pirates2.jpg' > Events are held bi-weekly hiring local talent only. Everything from minstrels to exotic dancers.`;
 				pageBody.appendChild(eventsContainer);
-
-				const renderEventsImage = (() => {
-					let eventsImage = document.createElement('img');
-					eventsImage.id = 'events-image';
-					eventsImage.src = '../dist/images/about/drinking-pirates2.jpg';
-					eventsImage.alt = 'events-image';
-					eventsContainer.appendChild(eventsImage);
-				})();
-
-				const renderEventsText = (() => {
-					let eventsText = document.createElement('div');
-					eventsText.id = 'events-text';
-					eventsText.innerText =
-						'Events are held bi-weekly hiring local talent only. Everything from minstrels to exotic dancers';
-					eventsContainer.appendChild(eventsText);
-				})();
 			})();
 
 			const renderLocationContainer = (() => {
 				let locationContainer = document.createElement('div');
 				locationContainer.id = 'location-container';
 				locationContainer.className = 'secondary-text';
+				locationContainer.innerHTML = `<img id='location-image' src='../dist/images/about/drinking-pirates3.jpg' > The Boozy Badger is located in the docks district in the city of Morath on the southern tip on the Isle of Valkenkrig.`;
 				pageBody.appendChild(locationContainer);
-
-				const renderLocationImage = (() => {
-					let locationImage = document.createElement('img');
-					locationImage.id = 'location-image';
-					locationImage.src = '../dist/images/about/drinking-pirates3.jpg';
-					locationImage.alt = 'location-image';
-					locationContainer.appendChild(locationImage);
-				})();
-
-				const renderLocationText = (() => {
-					let locationText = document.createElement('div');
-					locationText.id = 'location-text';
-					locationText.className = 'secondary-text';
-					locationText.innerText =
-						'The Boozy Badger is located in the docks district in the city of Morath on the southern tip of the Isle of Valkenkrig.';
-					locationContainer.appendChild(locationText);
-				})();
 			})();
 		})();
 	};

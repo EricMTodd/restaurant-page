2b337680b3473c95f733c90908ab8a5720ec5d7a
Eric M. Todd <eterictodd@gmail.com>
2021-04-20 02:13:19 -0600

    Committing for good measure.

--- src/contact.js
+++ src/contact.js
@@ -3,7 +3,14 @@ const contactPageController = (() => {
 		let pageBody = document.querySelector('#page-body');
 		pageBody.innerHTML = '';
 		const renderContactPageTitle = (() => {
-			document.querySelector('#page-title').innerText = 'Contact Us';
+			document.querySelector('#page-title').innerText = 'Contact us';
+		})();
+
+		const renderContactInformationContainer = (() => {
+			let contactInformationContainer = document.createElement('div');
+			contactInformationContainer.id = 'contact-information-container';
+			contactInformationContainer.innerHTML = `<div>The Boozy Badger has a coop of trained carrier pidgeons. Just send a pidgeon and we will ensure it's safety and health is in good standing. Simply address the note to "Caitlyn" our coop keeper and she will handle all inquirys or requests.</div>`;
+			pageBody.appendChild(contactInformationContainer);
 		})();
 	};
 	return {
--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,38 @@ const menuPageController = (() => {
 		const renderMenuPageTitle = (() => {
 			document.querySelector('#page-title').innerText = 'Our Menu';
 		})();
+
+		const renderDrinksMenu = (() => {
+			let drinksContainer = document.createElement('div');
+			drinksContainer.id = 'drinks-container';
+			drinksContainer.className = 'primary-text';
+			drinksContainer.innerText = 'Drinks';
+			pageBody.appendChild(drinksContainer);
+		})();
+
+		const renderBelgianAleContainer = (() => {
+			let belgianAleContainer = document.createElement('div');
+			belgianAleContainer.id = 'belgian-ale-container';
+			belgianAleContainer.innerHTML = `
+				<div id='belgian-ale-title' class='secondary-text'>Belgian Ale</div>
+				<div class='secondary-text'>8.00% ABV | $7.50</div>
+				`;
+			document
+				.querySelector('#drinks-container')
+				.appendChild(belgianAleContainer);
+		})();
+
+		const renderIndiaPaleAle = (() => {
+			let indiaPaleAleContainer = document.createElement('div');
+			indiaPaleAleContainer.id = 'india-pale-ale-container';
+			indiaPaleAleContainer.innerHTML = `
+				<div id='india-pale-ale-title' class='secondary-text'>India Pale Ale</div>
+				<div class='secondary-text'>6.5% ABV | $6.00</div>
+				`;
+			document
+				.querySelector('#drinks-container')
+				.appendChild(indiaPaleAleContainer);
+		})();
 	};
 	return {
 		renderMenuPage,

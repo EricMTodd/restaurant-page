c0c349b448291c5185cf97ca3d15bf45104cdb7f
Eric M. Todd <eterictodd@gmail.com>
2021-04-21 13:18:23 -0600

    Working on starters container for the menu.

--- src/menu.js
+++ src/menu.js
@@ -72,10 +72,18 @@ const menuPageController = (() => {
 			})();
 
 			const renderFriedPickles = (() => {
-				console.log('oops');
+				let friedPicklesContainer = document.createElement('div');
+				friedPicklesContainer.id = 'fried-pickles-container';
+
+				friedPicklesContainer.innerHTML = `
+				<div id='fried-pickles-title' class='secondary-text'>Fried Pickles</div>
+				<div class='secondary-text'>$4.00</div>
+				`;
+				startersContainer.appendChild(friedPicklesContainer);
 			})();
 		})();
 	};
+
 	return {
 		renderMenuPage,
 	};
